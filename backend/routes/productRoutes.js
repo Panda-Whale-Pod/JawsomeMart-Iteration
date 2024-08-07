983240850afaeafa3266e8207005aba9cdb42497
Seth Hollingsworth <sethhollingsworth@MacBook-Pro.local>
2024-08-07 14:38:29 -0700

    changes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {getProducts, getOneProduct} = require('../controllers/productController');
 
+
 router.get('/products', getProducts,(req,res)=>{
     res.status(200).json(res.locals.allProducts)
 })
